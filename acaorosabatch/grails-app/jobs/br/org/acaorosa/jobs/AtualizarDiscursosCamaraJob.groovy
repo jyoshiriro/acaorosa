106679452aad1d56d529d2cdaf19084699aaafe0
jyoshiriro <jyoshiriro@gmail.com>
2014-11-26 01:31:30 -0200

    Atualização geral 25/11

---
+++ acaorosabatch/grails-app/jobs/br/org/acaorosa/jobs/AtualizarDiscursosCamaraJob.groovy
@@ -0,0 +1,27 @@
+package br.org.acaorosa.jobs
+
+import br.org.acaorosa.services.atualizacoes.AtualizarDiscursoCamaraService;
+import br.org.acaorosa.services.atualizacoes.AtualizarProposicaoCamaraService
+import groovy.util.logging.Log4j;
+
+
+@Log4j
+class AtualizarDiscursosCamaraJob {
+	
+	def atualizarDiscursoCamaraService
+	
+    static triggers = {
+      simple repeatInterval: 550000l // execute job once in 5 seconds
+	  //cron name: "AtualizarDiscursosCamara", cronExpression: "* * 3 * * ?"
+    }
+
+    def execute() {
+        try {
+			atualizarDiscursoCamaraService.atualizar()
+			log.debug("Atualização Geral dos registros de Discursos concluída com sucesso")
+		} catch (Exception e) {
+			log.error("Erro ao tentar a Atualização Geral dos registros de Cadastro de Discursos: ${e.message}")
+			e.printStackTrace()
+		}
+    }
+}
---
+++ acaorosabatch/grails-app/jobs/br/org/acaorosa/jobs/AtualizarMensagensJob.groovy
@@ -0,0 +1,26 @@
+package br.org.acaorosa.jobs
+
+import br.org.acaorosa.services.atualizacoes.AtualizarMensagensService;
+
+
+
+class AtualizarMensagensJob {
+	
+    static triggers = {
+      simple repeatInterval: 15000l // execute job once in 5 seconds
+	  //cron name: "AtualizarMensagens", cronExpression: "* 55 5 * * ?"
+    }
+	
+	def atualizarMensagensService
+
+    def execute() {
+		
+		try {
+			atualizarMensagensService.atualizar()
+			log.debug("Geração de Mensagens concluída com sucesso")
+		} catch (Exception e) {
+			log.error("Erro ao tentar a gerar as mensagens: ${e.message}")
+			e.printStackTrace()
+		}
+    }
+}
